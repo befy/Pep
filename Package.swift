edf5b072aecb420d75accc8be38f30b9df2ff79e
befy <alper@tabak.app>
2020-11-05 14:51:00 +0300

    Sources dived into Pep folder

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Sources",
+            name: "Pep",
             dependencies: []),
         .testTarget(
             name: "PepTests",
